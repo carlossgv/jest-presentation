7579454069efc1b54a950ad4a1f62ba1bebe746a
Carlos Gonzalez <carlossgv@gmail.com>
2024-07-03 12:52:36 -0600

    test(repository): getPokemon

--- src/infra/pokeapi/pokeapi-pokemon.repository.spec.ts
+++ src/infra/pokeapi/pokeapi-pokemon.repository.spec.ts
@@ -1,12 +1,17 @@
-import axios from "axios";
 import PokeApiPokemonRepository from "./pokeapi-pokemon.repository";
+import Pokemon from "../../core/entities/pokemon.entity";
+import axios from "axios";
 jest.mock("axios");
+jest.mock("../../core/entities/pokemon.entity");
 
 describe("PokeapiPokemonRepository", () => {
   let repository: PokeApiPokemonRepository;
+  let axiosMock: jest.Mocked<typeof axios>;
+  let pokemonMock: jest.MockedClass<typeof Pokemon>;
   beforeEach(() => {
+    pokemonMock = Pokemon as jest.MockedClass<typeof Pokemon>;
+    axiosMock = axios as jest.Mocked<typeof axios>;
     repository = new PokeApiPokemonRepository();
-    (axios.get as jest.Mock).mockResolvedValue({});
   });
   afterEach(() => {
     jest.clearAllMocks();
@@ -14,7 +19,19 @@ describe("PokeapiPokemonRepository", () => {
 
   describe("getPokemon", () => {
     it("should return a Pokemon entity", async () => {
-      await repository.getPokemon("pikachu");
+      axiosMock.get.mockResolvedValue({
+        data: {
+          name: "pikachu",
+          types: [{ type: { name: "electric" } }],
+        },
+      });
+      const response = await repository.getPokemon("pikachu");
+
+      expect(pokemonMock).toHaveBeenCalledWith("pikachu", ["electric"]);
+      expect(pokemonMock).toHaveBeenCalledTimes(1);
+
+      expect(response).toBeInstanceOf(pokemonMock);
+      expect(response).toEqual(pokemonMock.mock.instances[0]);
     });
   });
 });
